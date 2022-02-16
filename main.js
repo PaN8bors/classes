9ada798c4f420a7d7e800d99cb7064b34afce501
Parish Neighbors <pn8bors@gmail.com>
2022-02-16 10:19:02 -0600

    Second commit

--- main.js
+++ main.js
@@ -1,13 +1,52 @@
 class Pet {
-    constructor(species, name) {
-        this.species = species;
+    constructor(name) {
+        this.species = null;
         this.name = name;
+        this.breed=null;
+        this.age=null;
+        this.gender=null;
+    }
+
+    setName(newName) {
+        this.name = newName;
+        return this;
+    }
+
+    setGender(gender) {
+        this.gender = gender;
+        return this;
+    }
+
+    setAge(age) {
+        this.age = age;
+        return this;
+    }
+
+    setBreed(breed) {
+        this.breed = breed;
+        return this;
+    }
+
+    setSpecies(species) {
+        this.species = species;
+        return this;
+    }
+
+    describe() {
+        console.log(
+            `This is my ${this.species} ${this.name}. ` 
+            + `He is a ${this.age} year old ${this.gender} ${this.breed}.`
+        )
     }
 }
 
 function main() {
-    const myPet = new Pet('male', 'Cooper');
-        console.log(myPet)
+    const myPet = new Pet('Cooper')
+        myPet.setSpecies('dog')
+             .setAge('20')
+             .setBreed('Shi Tzu')
+             .setGender('male')
+    myPet.describe();
 }
 
 main();
